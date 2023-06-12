aea303913652e771cd0e804255256dd9634e9011
arunrawat404 <arunrawatar404@gmail.com>
2023-06-12 22:53:22 +0530

    Added rate limiter to api gateway

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 const express = require("express");
+const rateLimit = require("express-rate-limit");
 
 const { ServerConfig } = require("./config");
 const apiRoutes = require("./routes");
 
 const app = express();
 
+const limiter = rateLimit({
+    windowMs: 2 * 60 * 1000, // 2 minutes
+    max: 4 // limit each IP to 4 requests per window
+})
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Apply the rate limiting middleware to all requests
+app.use(limiter);
 
 // whenever we get a url that starts with /api will redirect all request to apiRoutes
 app.use("/api", apiRoutes);
