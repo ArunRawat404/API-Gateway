3030ba81ea20f85fc5dd994f7d8c4379ac721908
arunrawat404 <arunrawatar404@gmail.com>
2023-06-12 23:35:08 +0530

    Added proxy

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-const express = require("express");
 const rateLimit = require("express-rate-limit");
+const express = require("express");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const { ServerConfig } = require("./config");
 const apiRoutes = require("./routes");
@@ -17,10 +18,27 @@ app.use(express.urlencoded({ extended: true }));
 // Apply the rate limiting middleware to all requests
 app.use(limiter);
 
+/** 
+Proxy
+Client -> API Gateway(localhost:5000) -> Flight Service or Booking Service
+**/
+app.use("/flightsService", createProxyMiddleware({
+    target: ServerConfig.FLIGHT_SERVICE,
+    changeOrigin: true,
+    pathRewrite: { "^/flightsService": "/" }
+}));
+app.use("/bookingService", createProxyMiddleware({
+    target: ServerConfig.BOOKING_SERVICE,
+    changeOrigin: true,
+    pathRewrite: { "^/bookingService": "/" }
+}));
+
 // whenever we get a url that starts with /api will redirect all request to apiRoutes
 app.use("/api", apiRoutes);
 
 app.listen(ServerConfig.PORT, () => {
     console.log(`Server is up and running on PORT ${ServerConfig.PORT}`);
 });
+
+
 
